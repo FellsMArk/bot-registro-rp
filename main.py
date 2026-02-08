4adbeece65e3765f15073916487386dedbd8ffca
FellsMArk <58790483+FellsMArk@users.noreply.github.com>
2026-02-07 21:13:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
-TOKEN = os.getenv("MTQ2OTI5NTA5Njg3MTc4MDQ2NQ.G957HJ.rtlnjq1rewaSc7Hx_myiBhAE8TxTJEO1dnQi6U")
-
+TOKEN = os.getenv("token")
+ 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
@@ -153,4 +153,4 @@ async def painel(interaction: discord.Interaction):
     await interaction.response.send_message("Painel criado.", ephemeral=True)
 
 
-bot.run("MTQ2OTI5NTA5Njg3MTc4MDQ2NQ.G957HJ.rtlnjq1rewaSc7Hx_myiBhAE8TxTJEO1dnQi6U")
+bot.run(token)
